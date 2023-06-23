5a3bef5a5520adadc2c0c5b48713a7088fc07355
ada.young <735529126@qq.com>
2023-06-23 10:07:49 +0800

    增加shiro

---
+++ adapters/security/src/main/java/com/nbsaas/boot/security/token/DataToken.java
@@ -0,0 +1,20 @@
+package com.nbsaas.boot.security.token;
+
+import lombok.Data;
+import org.apache.shiro.authc.UsernamePasswordToken;
+
+import java.util.List;
+
+@Data
+public class DataToken<T> extends UsernamePasswordToken {
+
+    private T data;
+
+    private List<String> permissions;
+
+    private List<String> roles;
+
+    public DataToken() {
+        super("ada", "ada".toCharArray());
+    }
+}
