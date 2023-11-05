ca34e28daa2a6a9e41bc126e6ae0cc9fd8d0f51b
ada.young <735529126@qq.com>
2023-11-05 13:59:47 +0800

    修改菜单移动功能

--- gates/admins/system-admin/src/main/java/com/nbsaas/boot/controller/system/ApplicationController.java
+++ gates/admins/system-admin/src/main/java/com/nbsaas/boot/controller/system/ApplicationController.java
@@ -10,6 +10,7 @@ import com.nbsaas.boot.system.api.domain.request.ApplicationSearchRequest;
 import com.nbsaas.boot.system.api.domain.response.ApplicationResponse;
 import com.nbsaas.boot.system.api.domain.simple.ApplicationSimple;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,12 +29,13 @@ public class ApplicationController {
     @Resource
     private ApplicationApi applicationApi;
 
-
+    @RequiresPermissions("application")
     @RequestMapping("/search")
     public PageResponse<ApplicationSimple> search(ApplicationSearchRequest request) {
         return applicationApi.search(request);
     }
 
+    @RequiresPermissions("application")
     @RequestMapping("/list")
     public ListResponse<ApplicationSimple> list(ApplicationSearchRequest request) {
         return applicationApi.list(request);
@@ -45,6 +47,7 @@ public class ApplicationController {
      * @param request
      * @return
      */
+    @RequiresPermissions("application")
     @CreateData
     @RequestMapping("/create")
     public ResponseObject
@@ -52,17 +55,20 @@ public class ApplicationController {
         return applicationApi.create(request);
     }
 
+    @RequiresPermissions("application")
     @UpdateData
     @RequestMapping("/update")
     public ResponseObject<ApplicationResponse> update(@Validated(UpdateOperator.class) ApplicationDataRequest request) {
         return applicationApi.update(request);
     }
 
+    @RequiresPermissions("application")
     @RequestMapping("/delete")
     public ResponseObject<?> delete(@Validated(DeleteOperator.class) ApplicationDataRequest request) {
         return applicationApi.delete(request);
     }
 
+    @RequiresPermissions("application")
     @RequestMapping("/view")
     public ResponseObject<ApplicationResponse> view(@Validated(ViewOperator.class) ApplicationDataRequest request) {
         return applicationApi.view(request);
--- gates/admins/system-admin/src/main/java/com/nbsaas/boot/controller/system/MenuController.java
+++ gates/admins/system-admin/src/main/java/com/nbsaas/boot/controller/system/MenuController.java
@@ -10,9 +10,11 @@ import com.nbsaas.boot.system.api.domain.request.MenuSearchRequest;
 import com.nbsaas.boot.system.api.domain.response.MenuResponse;
 import com.nbsaas.boot.system.api.domain.simple.MenuSimple;
 import com.nbsaas.boot.system.ext.apis.MenuExtApi;
+import com.nbsaas.boot.system.ext.domain.request.MenuDragRequest;
 import com.nbsaas.boot.system.ext.domain.request.UpdateRoleMenuRequest;
 import com.nbsaas.boot.system.ext.domain.simple.MenuExtSimple;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,40 +38,49 @@ public class MenuController {
     @Resource
     private MenuExtApi menuExtApi;
 
+    @RequiresPermissions("menu")
+    @RequestMapping("/drag")
+    public ResponseObject<?> drag(MenuDragRequest menuDragRequest) {
+        return menuExtApi.drag(menuDragRequest);
+    }
 
+    @RequiresPermissions("menu")
     @RequestMapping("/root")
     public ListResponse<MenuExtSimple> root() {
         return menuExtApi.root();
     }
 
-
+    @RequiresPermissions("menu")
     @RequestMapping("/findByRole")
     public ListResponse<MenuExtSimple> findByRole(Long role) {
         return menuExtApi.findByRole(role);
     }
 
-
+    @RequiresPermissions("menu")
     @RequestMapping("/selectForPermission")
     public ListResponse<Long> selectForPermission(Long role) {
         return menuExtApi.selectForPermission(role);
     }
 
+    @RequiresPermissions("menu")
     @RequestMapping("/selectPermissionByUser")
     public ListResponse<String> selectPermissionByUser(Long userId) {
         return menuExtApi.selectPermissionByUser(userId);
     }
 
+    @RequiresPermissions("menu")
     @RequestMapping("/updateRoleMenus")
     public ResponseObject<?> updateRoleMenus(@RequestBody @Validated UpdateRoleMenuRequest request) {
         return menuExtApi.updateRoleMenus(request);
     }
 
-
+    @RequiresPermissions("menu")
     @RequestMapping("/search")
     public PageResponse<MenuSimple> search(MenuSearchRequest request) {
         return menuApi.search(request);
     }
 
+    @RequiresPermissions("menu")
     @RequestMapping("/list")
     public ListResponse<MenuSimple> list(MenuSearchRequest request) {
         return menuApi.list(request);
@@ -81,6 +92,7 @@ public class MenuController {
      * @param request
      * @return
      */
+    @RequiresPermissions("menu")
     @CreateData
     @RequestMapping("/create")
     public ResponseObject
@@ -88,17 +100,20 @@ public class MenuController {
         return menuApi.create(request);
     }
 
+    @RequiresPermissions("menu")
     @UpdateData
     @RequestMapping("/update")
     public ResponseObject<MenuResponse> update(@Validated(UpdateOperator.class) MenuDataRequest request) {
         return menuApi.update(request);
     }
 
+    @RequiresPermissions("menu")
     @RequestMapping("/delete")
     public ResponseObject<?> delete(@Validated(DeleteOperator.class) MenuDataRequest request) {
         return menuApi.delete(request);
     }
 
+    @RequiresPermissions("menu")
     @RequestMapping("/view")
     public ResponseObject<MenuResponse> view(@Validated(ViewOperator.class) MenuDataRequest request) {
         return menuApi.view(request);
